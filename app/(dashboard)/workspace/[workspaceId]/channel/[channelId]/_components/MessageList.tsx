f3bcc118b875ec26a2432e25adf1eafb8707147e
karas011029-svg <karas011029@gmail.com>
2025-11-08 22:22:36 +0530

    feat(messages): implement cursor-based pagination for infinite message loading

--- app/(dashboard)/workspace/[workspaceId]/channel/[channelId]/_components/MessageList.tsx
+++ app/(dashboard)/workspace/[workspaceId]/channel/[channelId]/_components/MessageList.tsx
@@ -1,26 +1,43 @@
 "use client";
 
-import { useQuery } from "@tanstack/react-query";
+import { useInfiniteQuery, useQuery } from "@tanstack/react-query";
 import MessageItem from "./message/MessageItem";
 import { orpc } from "@/lib/orpc";
 import { useParams } from "next/navigation";
+import { useMemo } from "react";
 
 const MessageList = () => {
   const { channelId } = useParams<{ channelId: string }>();
 
-  const { data } = useQuery(
-    orpc.message.list.queryOptions({
-      input: {
-        channelId,
-      },
-    })
-  );
+  const infiniteOptions = orpc.message.list.infiniteOptions({
+    input: (pageParam: string | undefined) => ({
+      channelId: channelId,
+      cursor: pageParam,
+      limit: 30,
+    }),
+    initialPageParam: undefined,
+    getNextPageParam: (lastPage) => lastPage.nextCursor,
+    select: (data) => ({
+      pages: [...data.pages].reverse(),
+      pageParams: [...data.pageParams].reverse(),
+    }),
+  });
+
+  const query = useInfiniteQuery({
+    ...infiniteOptions,
+    staleTime: 30_000,
+    refetchOnWindowFocus: false,
+  });
+
+  const items = useMemo(() => {
+    return query.data?.pages.flatMap((p) => p.items) ?? [];
+  }, [query.data]);
 
   return (
     <>
       <div className="relative h-full">
         <div className="h-full overflow-y-auto px-4">
-          {data?.map((msg) => (
+          {items?.map((msg) => (
             <MessageItem key={msg.id} message={msg} />
           ))}
         </div>
