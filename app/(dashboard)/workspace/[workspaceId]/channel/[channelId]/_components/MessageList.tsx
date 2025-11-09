c84701c1bf5b8f88961721f48c61d388bc18d6fc
karas011029-svg <karas011029@gmail.com>
2025-11-09 22:19:58 +0530

    feat(messages): implement infinite scroll and scroll-to-bottom behavior in message list

--- app/(dashboard)/workspace/[workspaceId]/channel/[channelId]/_components/MessageList.tsx
+++ app/(dashboard)/workspace/[workspaceId]/channel/[channelId]/_components/MessageList.tsx
@@ -1,13 +1,22 @@
 "use client";
 
-import { useInfiniteQuery, useQuery } from "@tanstack/react-query";
+import { useInfiniteQuery } from "@tanstack/react-query";
 import MessageItem from "./message/MessageItem";
 import { orpc } from "@/lib/orpc";
 import { useParams } from "next/navigation";
-import { useMemo } from "react";
+import { useEffect, useMemo, useRef, useState } from "react";
+import { Button } from "@/components/ui/button";
 
 const MessageList = () => {
   const { channelId } = useParams<{ channelId: string }>();
+
+  const [hasInitialScrolled, setHasInitialScrolled] = useState(false);
+  const scrollRef = useRef<HTMLDivElement | null>(null);
+
+  const bottomRef = useRef<HTMLDivElement | null>(null);
+  const [isAtBottom, setIsAtBottom] = useState(false);
+  const [newMessages, setNewMessages] = useState(false);
+  const lastItemIdRef = useRef<string | undefined>(undefined);
 
   const infiniteOptions = orpc.message.list.infiniteOptions({
     input: (pageParam: string | undefined) => ({
@@ -18,35 +27,130 @@ const MessageList = () => {
     initialPageParam: undefined,
     getNextPageParam: (lastPage) => lastPage.nextCursor,
     select: (data) => ({
-      pages: [...data.pages].reverse(),
-      //  pages: [...data.pages]
-      //   .map((p) => ({
-      //     ...p,
-      //     items: [...p.items].reverse(),
-      //   }))
-      //   .reverse(),
+      // pages: [...data.pages].reverse(),
+      pages: [...data.pages]
+        .map((p) => ({
+          ...p,
+          items: [...p.items].reverse(),
+        }))
+        .reverse(),
       pageParams: [...data.pageParams].reverse(),
     }),
   });
 
-  const query = useInfiniteQuery({
+  const {
+    data,
+    fetchNextPage,
+    hasNextPage,
+    isFetchingNextPage,
+    isLoading,
+    error,
+    isFetching,
+  } = useInfiniteQuery({
     ...infiniteOptions,
     staleTime: 30_000,
     refetchOnWindowFocus: false,
   });
 
+  useEffect(() => {
+    if (!hasInitialScrolled && data?.pages.length) {
+      const el = scrollRef.current;
+
+      if (el) {
+        el.scrollTop = el.scrollHeight;
+        setHasInitialScrolled(true);
+        setIsAtBottom(true);
+      }
+    }
+  }, [hasInitialScrolled, data?.pages.length]);
+
+  const isNearBottom = (el: HTMLDivElement) =>
+    el.scrollHeight - el.scrollTop - el.clientHeight <= 80;
+
+  const handleScroll = () => {
+    const el = scrollRef.current;
+
+    if (!el) return;
+
+    if (el.scrollTop <= 80 && hasNextPage && !isFetching) {
+      const prevScrollHeight = el.scrollHeight;
+      const prevScrollTop = el.scrollTop;
+
+      fetchNextPage().then(() => {
+        const newScrollHeight = el.scrollHeight;
+
+        el.scrollTop = newScrollHeight - prevScrollHeight + prevScrollTop;
+      });
+    }
+
+    setIsAtBottom(isNearBottom(el));
+  };
+
   const items = useMemo(() => {
-    return query.data?.pages.flatMap((p) => p.items) ?? [];
-  }, [query.data]);
+    return data?.pages.flatMap((p) => p.items) ?? [];
+  }, [data]);
+
+  useEffect(() => {
+    if (!items.length) return;
+
+    const lastId = items[items.length - 1].id;
+
+    const prevLastId = lastItemIdRef.current;
+
+    const el = scrollRef.current;
+
+    if (prevLastId && lastId !== prevLastId) {
+      if (el && isNearBottom(el)) {
+        requestAnimationFrame(() => {
+          el.scrollTop = el.scrollHeight;
+        });
+
+        setNewMessages(false);
+        setIsAtBottom(true);
+      } else {
+        setNewMessages(true);
+      }
+    }
+
+    lastItemIdRef.current = lastId;
+  }, [items]);
+
+  const scrollToBottom = () => {
+    const el = scrollRef.current;
+
+    if (!el) return;
+
+    el.scrollTop = el.scrollHeight;
+
+    setNewMessages(false);
+    setIsAtBottom(true);
+  };
 
   return (
     <>
       <div className="relative h-full">
-        <div className="h-full overflow-y-auto px-4">
+        <div
+          className="h-full overflow-y-auto px-4"
+          ref={scrollRef}
+          onScroll={handleScroll}
+        >
           {items?.map((msg) => (
             <MessageItem key={msg.id} message={msg} />
           ))}
+
+          <div ref={bottomRef}></div>
         </div>
+
+        {newMessages && !isAtBottom ? (
+          <Button
+            type="button"
+            className="absolute bottom-4 right-8 rounded-full"
+            onClick={scrollToBottom}
+          >
+            {" "}
+            New Messages
+          </Button>
+        ) : null}
       </div>
     </>
   );
