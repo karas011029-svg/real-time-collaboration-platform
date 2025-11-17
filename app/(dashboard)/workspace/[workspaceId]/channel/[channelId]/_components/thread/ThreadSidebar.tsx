6c3317d1d94d6c6e0cd1949bcda04f04297de700
karas011029-svg <karas011029@gmail.com>
2025-11-17 22:49:01 +0530

    feat(threads): add ThreadSidebar UI and update Prisma schema for threaded messages

--- app/(dashboard)/workspace/[workspaceId]/channel/[channelId]/_components/thread/ThreadSidebar.tsx
+++ app/(dashboard)/workspace/[workspaceId]/channel/[channelId]/_components/thread/ThreadSidebar.tsx
@@ -1,5 +1,39 @@
 import { Button } from "@/components/ui/button";
 import { MessageSquare, X } from "lucide-react";
+import Image from "next/image";
+
+const messages = [
+  {
+    id: 1,
+    authorName: "Dev Sethi",
+    authorImage: "https://avatars.githubusercontent.com/u/147725410?v=4",
+    content: "Something is missing in this thread",
+    createdAt: new Date(),
+  },
+  {
+    id: 2,
+    authorName: "John Doe",
+    authorImage: "https://avatars.githubusercontent.com/u/147725410?v=4",
+    content:
+      "Hey there! I'm a Frontend Developer with a passion for crafting slick, user-friendly interfaces.",
+    createdAt: new Date(),
+  },
+  {
+    id: 3,
+    authorName: "Nathan Fisher",
+    authorImage: "https://avatars.githubusercontent.com/u/147725410?v=4",
+    content: "Building web apps that not only look great",
+    createdAt: new Date(),
+  },
+  {
+    id: 4,
+    authorName: "John Snow",
+    authorImage: "https://avatars.githubusercontent.com/u/147725410?v=4",
+    content:
+      "Whether it's making websites more responsive or creating intuitive user experiences, I'm your go-to dev!",
+    createdAt: new Date(),
+  },
+];
 
 const ThreadSidebar = () => {
   return (
@@ -21,7 +55,34 @@ const ThreadSidebar = () => {
 
         {/* Main Content */}
         <div className="flex-1 overflow-y-auto">
-            
+          <div className="p-4 border-b bg-muted-20">
+            <div className="flex space-x-3">
+              <Image
+                src={messages[0].authorImage}
+                alt="Author Image"
+                width={32}
+                height={32}
+                className="size-8 rounded-full shrink-0"
+              />
+
+              <div className="flex-1 space-y-1 min-w-0">
+                <div className="flex items-center space-x-2">
+                  <span className="font-medium text-sm">
+                    {messages[0].authorName}
+                  </span>
+                  <span className="text-xs text-muted-foreground">
+                    {new Intl.DateTimeFormat("en-US", {
+                      hour: "numeric",
+                      minute: "numeric",
+                      hour12: true,
+                      month: "short",
+                      day: "numeric",
+                    }).format(messages[0].createdAt)}
+                  </span>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </>
