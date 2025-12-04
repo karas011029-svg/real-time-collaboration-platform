3134a0983744d9a8e8abea13e3c6d49c8c3c946b
karas011029-svg <karas011029@gmail.com>
2025-12-04 18:14:04 +0530

    feat(realtime): add user presence system using PartyKit server

---
+++ hooks/use-presence.ts
@@ -0,0 +1,57 @@
+import { useState } from "react";
+import usePartySocket from "partysocket/react";
+import {
+  PresenceMessageSchema,
+  PresenceMessageSchemaType,
+  User,
+} from "@/app/schemas/realtime";
+
+interface usePresenceProps {
+  room: string;
+  currentUser: User | null;
+}
+
+export function usePresence({ room, currentUser }: usePresenceProps) {
+  const [onlineUsers, setOnlineUsers] = useState<User[]>([]);
+
+  const socket = usePartySocket({
+    host: "",
+    room: room,
+    party: "chat",
+    onOpen() {
+      console.log("Connected to presence room:", room);
+
+      if (currentUser) {
+        const message: PresenceMessageSchemaType = {
+          type: "add-user",
+          payload: currentUser,
+        };
+
+        socket.send(JSON.stringify(message));
+      }
+    },
+    onMessage(event) {
+      try {
+        const message = JSON.parse(event.data);
+        const result = PresenceMessageSchema.safeParse(message);
+
+        if (result.success && result.data.type === "presence") {
+          setOnlineUsers(result.data.payload.users);
+        }
+      } catch (error) {
+        console.log("Failed to parse message", error);
+      }
+    },
+    onClose() {
+      console.log("Disconnected from presence room", room);
+    },
+    onError(error) {
+      console.error("Websocket Error", error);
+    },
+  });
+
+  return {
+    onlineUsers,
+    socket,
+  };
+}
---
+++ realtime/index.ts
@@ -0,0 +1,122 @@
+import { PresenceMessageSchema, UserSchema } from "@/app/schemas/realtime";
+import { Connection, routePartykitRequest, Server } from "partyserver";
+import z from "zod";
+
+type Env = { Chat: DurableObjectNamespace<Chat> };
+
+const ConnectionStateSchema = z
+  .object({
+    user: UserSchema.nullable().optional(),
+  })
+  .nullable();
+
+type ConnectionState = z.infer<typeof ConnectionStateSchema>;
+
+type Message = z.infer<typeof PresenceMessageSchema>;
+
+// Define your Server
+export class Chat extends Server {
+  static options: {
+    hibernate: true;
+  };
+
+  onConnect(connection: Connection) {
+    console.log("Connected", connection.id, "to server", this.name);
+
+    // Send current presence to the newly connected user
+    connection.send(JSON.stringify(this.getPresenceMessage()));
+  }
+
+  onClose(connection: Connection) {
+    console.log(`User disconnected: ${connection.id}`);
+
+    this.updateUsers();
+  }
+
+  onError(connection: Connection) {
+    console.log(`Connection Error: ${connection.id}`);
+
+    this.updateUsers();
+  }
+
+  onMessage(connection: Connection, message: string) {
+    try {
+      const parsed = JSON.parse(message);
+      const presence = PresenceMessageSchema.safeParse(parsed);
+
+      if (presence.success) {
+        if (presence.data.type === "add-user") {
+          // store user info on the connection
+          this.setConnectionState(connection, { user: presence.data.payload });
+
+          // Broadcast updated presence to all the clients
+
+          this.updateUsers();
+
+          return;
+        }
+      }
+      if (presence.data?.type === "remove-user") {
+        this.setConnectionState(connection, null);
+
+        this.updateUsers();
+
+        return;
+      }
+    } catch (error) {
+      console.log("Error processing message", error);
+    }
+  }
+
+  updateUsers() {
+    const presenceMessage = JSON.stringify(this.getPresenceMessage());
+
+    // Use partyservers build in broadcast
+    this.broadcast(presenceMessage);
+  }
+
+  getPresenceMessage() {
+    return {
+      type: "presence",
+      payload: { users: this.getUsers() },
+    } satisfies Message;
+  }
+
+  getUsers() {
+    const users = new Map();
+
+    for (const connection of this.getConnections()) {
+      const state = this.getConnectionState(connection);
+
+      if (state?.user) {
+        users.set(state.user.id, state.user);
+      }
+    }
+
+    return Array.from(users.values());
+  }
+
+  private setConnectionState(connection: Connection, state: ConnectionState) {
+    connection.setState(state);
+  }
+
+  private getConnectionState(connection: Connection): ConnectionState {
+    const result = ConnectionStateSchema.safeParse(connection.state);
+
+    if (result.success) {
+      return result.data;
+    }
+
+    return null;
+  }
+}
+
+export default {
+  // Set up your fetch handler to use configured Servers
+  async fetch(request: Request, env: Env): Promise<Response> {
+    return (
+      (await routePartykitRequest(request, env)) ||
+      new Response("Not Found", { status: 404 })
+    );
+  },
+} satisfies ExportedHandler<Env>;
